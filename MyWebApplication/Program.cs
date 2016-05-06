254335b8da68dde8bfb86b635313878a24c29b46
David Fowler <davidfowl@gmail.com>
2016-05-06 09:05:30 -0700

    Initial commit

---
+++ MyWebApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MyWebApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseStartup<Startup>()
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ MyWebApplication/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MyWebApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync($"Hello World!");
+            });
+        }
+    }
+}
